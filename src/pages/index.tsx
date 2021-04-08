e8a6c6138a5d362207fcd70ea3536793c8c23d12
Dustin Lacewell <dlacewell@gmail.com>
2021-04-08 06:32:49 -0500

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,70 @@
+import { Box } from "@chakra-ui/layout";
+import { Canvas, useFrame } from "@react-three/fiber";
+
+import SomeText from "components/SomeText";
+import SomeImage from "components/SomeImage";
+import CTASection from "components/CTASection";
+import React, { FC } from "react";
+
+import { useRef } from "react";
+import { Torus } from "@react-three/drei";
+import { TorusBufferGeometry, Vector3 } from "three";
+import ThreeView from "../../node_modules/react-ecs/dist/extras/three/components/ThreeView";
+import { Entity, Facet } from "../../node_modules/react-ecs/dist/";
+import { useQuery, useSystem, useECS } from "../../node_modules/react-ecs/dist/hooks";
+
+class Spinning extends Facet<Spinning> {
+  rotation = new Vector3(0, 0, 0);
+}
+
+const SpinningSystem = () => {
+  const query = useQuery(e => e.hasAll(ThreeView, Spinning));
+
+  return useSystem((dt: number) => {
+      query.loop([ThreeView, Spinning], (e, [view, spin]) => {
+          const transform = view.ref.current!;
+          const rotation = spin.rotation.clone().multiplyScalar(dt);
+          const newRotation = transform.rotation
+              .toVector3()
+              .add(rotation);
+          transform.rotation.setFromVector3(newRotation);
+      });
+  });
+};
+
+const SpinningCube = () => {
+  return (
+      <Entity>
+          <Spinning rotation={new Vector3(1, 1, 1)} />
+          <ThreeView>
+              <Torus />
+          </ThreeView>
+      </Entity>
+  );
+};
+
+
+export const SpinningCubeStory: FC = () => {
+  const ECS = useECS();
+
+  useFrame((_, dt) => ECS.update(dt));
+
+  return (
+      <ECS.Provider>
+          <SpinningSystem />
+          <SpinningCube />
+      </ECS.Provider>
+  );
+};
+
+const Home = () => {
+  return (
+    <Box mb={8} w="full">
+      <Canvas>
+        <SpinningCubeStory />
+      </Canvas>
+    </Box>
+  );
+};
+
+export default Home;
